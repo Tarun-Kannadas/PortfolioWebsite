cd2e56022d36c35540d9a375eb33e7f0d7d7a63d
Tarun Kannadas <tarunkannadas@gmail.com>
2025-09-29 20:11:40 +0530

    update commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,26 @@
 function validationCheck(event) {
     event.preventDefault();
   
-    const name = document.getElementById('name').value.trim();
-    const phone = document.getElementById('phone').value.trim();
-    const place = document.getElementById('place').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const message = document.getElementById('message').value.trim();
+    Nname = document.getElementById('name').value.trim();
+    phone = document.getElementById('phone').value.trim();
+    place = document.getElementById('place').value.trim();
+    email = document.getElementById('email').value.trim();
+    message = document.getElementById('message').value.trim();
   
-    if (!name || !phone || !place || !email || !message) {
-      alert('Please fill in all fields.');
+    if (!Nname || !phone || !place || !email || !message) {
+      document.getElementById('warning').innerHTML = "Please Provide all the credentials to proceed with Submitting the form"
       return;
     }
+
+    document.getElementById('warning').innerHTML = null;
 }
-  
+
+function clearMsg()
+{
+  document.getElementById('name').value = ""
+  document.getElementById('phone').value = ""
+  document.getElementById('place').value = ""
+  document.getElementById('email').value = ""
+  document.getElementById('message').value = ""
+  document.getElementById('warning').innerHTML = null;
+}
